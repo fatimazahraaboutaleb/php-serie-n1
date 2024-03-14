e70ef47c0a4c4c7149d05f37e3611a218957664e
fatima zahra <fma87879@gmail.com>
2024-03-14 16:22:05 +0000

    Update ex5.php

--- ex5.php
+++ ex5.php
@@ -1,15 +1,17 @@
 <?php
  $tr=2;
 
- if ($tr==1){
-     echo "Contient divers outils pour dessiner : du papier, des crayons, etc.";
- }
- elseif ($tr==2){
-     echo "Contient du matériel informatique : des câbles, des composants, etc.";
- }
- elseif ($tr==3){
-     echo "Ah ? Ce tiroir est fermé à clé ! Dommage !";
- }
- elseif ($tr==4){
-     echo "Contient des vêtements : des chemises, des pantalons, etc.";
+ switch ($tr){
+    case "1":
+        echo "Contient divers outils pour dessiner : du papier, des crayons, etc.";
+        break;
+    case "2":
+        echo "Contient du matériel informatique : des câbles, des composants, etc.";
+        break;
+    case "3":
+        echo "Ah ? Ce tiroir est fermé à clé ! Dommage !";
+        break;
+    case "4":
+        echo "Contient des vêtements : des chemises, des pantalons, etc.";
+        break;
  }
